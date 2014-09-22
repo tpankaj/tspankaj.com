e48e2b0bfe2e416628852e01a23791c87859e17d
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2014-09-22 11:35:41 -0700

    Revert "Reverted to older version to see if it works"
    This reverts commit d040c0ac65f16935d13abb0af260278c58a0bb7d.

--- tspankaj/views.py
+++ tspankaj/views.py
@@ -1,2 +1,7 @@
-from flask import *
-from flask.ext.mongoengine import *
+#from flask import *
+#from flask.ext.mongoengine import *
+from . import *
+
+@app.route('/')
+def index():
+    return 'Hello, world!'
